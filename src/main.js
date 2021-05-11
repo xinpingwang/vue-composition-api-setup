0b911d5747aad64bedc5a3e658ce44ec01106a43
XinPing Wang <xinping.wang@hotmail.com>
2021-05-11 20:34:24 +0800

    feat: reproduce vuejs/composition-api#693

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import VueCompositionAPI from '@vue/composition-api'
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(VueCompositionAPI);
 
 new Vue({
   render: function (h) { return h(App) },
